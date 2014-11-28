fd66f6ebe0b6f97caf0701ef8e6cf0d93f52b789
Scott Busche <swbusche@gmail.com>
2014-11-27 23:31:06 -0600

    Pass the text to a new activity

--- app/src/main/java/com/busche/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/busche/myfirstapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.busche.myfirstapplication;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,9 +11,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "com.busche.myfirstapplication.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,17 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     /**
