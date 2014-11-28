1fd46e14dbf90a3500ad5ad612c32f7890f3b361
Scott Busche <swbusche@gmail.com>
2014-11-27 23:34:13 -0600

    Remove unused imports

--- app/src/main/java/com/busche/myfirstapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/busche/myfirstapplication/DisplayMessageActivity.java
@@ -1,13 +1,9 @@
 package com.busche.myfirstapplication;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 
