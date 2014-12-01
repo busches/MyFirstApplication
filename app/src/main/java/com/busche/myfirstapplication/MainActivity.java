df3c9c30e8a8866bf9cb275c27596d00211942bc
Scott Busche <swbusche@gmail.com>
2014-11-30 21:01:11 -0600

    Update onOptionsItemSelected for new menu option
    Not actually doing anything with it yet though

--- app/src/main/java/com/busche/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/busche/myfirstapplication/MainActivity.java
@@ -36,14 +36,12 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
